9d840751823c60785162e5104c7755514f3767fa
Mark deVilliers <mark.devilliers@adarga.ai>
2022-08-23 12:09:55 +0100

    extract out method

--- pkg/providers/images/kustomize.go
+++ pkg/providers/images/kustomize.go
@@ -26,40 +26,49 @@ func (k *kustomize) Images(ctx context.Context) (util.Set[string], error) {
 
 	for _, root := range k.paths {
 
-		// run kustomize in root - get back big ball of yaml
-		output, err := exec.GetCommandOutput(root, "kustomize", "build")
+		elements, err := runKustomizeAndSelect(root, "$..spec.containers[*].image")
 		if err != nil {
-			return all, errors.Wrap(err, "error running kustomize")
+			return all, err
 		}
-		// split out to the individual documents
-		yamls := strings.Split(output, "\n---\n")
-
-		for _, yamlstr := range yamls {
-			// extract all the .image values
-			var n yaml.Node
-
-			if err := yaml.Unmarshal([]byte(yamlstr), &n); err != nil {
-				return all, errors.Wrap(err, "error unmarshalling kustomize output")
-			}
-
-			path, err := yamlpath.NewPath("$..spec.containers[*].image")
-			if err != nil {
-				return all, errors.Wrap(err, "error creating yaml path")
-			}
-
-			elements, err := path.Find(&n)
-			if err != nil {
-				return all, errors.Wrap(err, "error finding image nodes")
-			}
-
-			// add to all keeping count
-			for _, element := range elements {
-				i := strings.TrimSpace(element.Value)
-				if i != "" {
-					all.Add(i)
-				}
+		// add to all keeping count
+		for _, element := range elements {
+			i := strings.TrimSpace(element.Value)
+			if i != "" {
+				all.Add(i)
 			}
 		}
 	}
 	return all, nil
+}
+
+func runKustomizeAndSelect(directory, xpath string) ([]*yaml.Node, error) {
+	ret := []*yaml.Node{}
+
+	// run kustomize in root - get back big ball of yaml
+	output, err := exec.GetCommandOutput(directory, "kustomize", "build")
+	if err != nil {
+		return nil, errors.Wrap(err, "error running kustomize")
+	}
+	// split out to the individual documents
+	yamls := strings.Split(output, "\n---\n")
+
+	for _, yamlstr := range yamls {
+		// extract all the .image values
+		var n yaml.Node
+
+		if err := yaml.Unmarshal([]byte(yamlstr), &n); err != nil {
+			return nil, errors.Wrap(err, "error unmarshalling kustomize output")
+		}
+
+		path, err := yamlpath.NewPath(xpath)
+		if err != nil {
+			return nil, errors.Wrap(err, "error creating yaml path")
+		}
+		elements, err := path.Find(&n)
+		if err != nil {
+			return nil, errors.Wrap(err, "error finding image nodes")
+		}
+		ret = append(ret, elements...)
+	}
+	return ret, nil
 }

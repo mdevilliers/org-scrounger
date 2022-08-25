dfe954e352d2b11b02c35cfe80920fa298e0c4d7
Mark deVilliers <mark.devilliers@adarga.ai>
2022-08-25 12:59:03 +0100

    chore: cherry pick changes from a doomed branch (#80)
    * refactor for clarity
    * move templating funcs to the output module

--- pkg/cmds/output/funcs.go
+++ pkg/cmds/output/funcs.go
@@ -1,4 +1,4 @@
-package funcs
+package output
 
 import (
 	"text/template"
--- pkg/providers/images/kustomize.go
+++ pkg/providers/images/kustomize.go
@@ -25,41 +25,46 @@ func (k *kustomize) Images(ctx context.Context) (util.Set[string], error) {
 	all := util.NewSet[string]()
 
 	for _, root := range k.paths {
-
-		// run kustomize in root - get back big ball of yaml
-		output, err := exec.GetCommandOutput(root, "kustomize", "build")
-		if err != nil {
-			return all, errors.Wrap(err, "error running kustomize")
-		}
-		// split out to the individual documents
-		yamls := strings.Split(output, "\n---\n")
-
-		for _, yamlstr := range yamls {
-			// extract all the .image values
-			var n yaml.Node
-
-			if err := yaml.Unmarshal([]byte(yamlstr), &n); err != nil {
-				return all, errors.Wrap(err, "error unmarshalling kustomize output")
-			}
-
-			path, err := yamlpath.NewPath("$..spec.containers[*].image")
-			if err != nil {
-				return all, errors.Wrap(err, "error creating yaml path")
-			}
-
-			elements, err := path.Find(&n)
-			if err != nil {
-				return all, errors.Wrap(err, "error finding image nodes")
-			}
-
-			// add to all keeping count
-			for _, element := range elements {
-				i := strings.TrimSpace(element.Value)
-				if i != "" {
-					all.Add(i)
-				}
-			}
+		if err := runKustomizeAndSelect(root, "$..spec.containers[*].image", all); err != nil {
+			return nil, err
 		}
 	}
 	return all, nil
+}
+
+func runKustomizeAndSelect(directory, xpath string, set util.Set[string]) error {
+
+	// run kustomize in root - get back big ball of yaml
+	output, err := exec.GetCommandOutput(directory, "kustomize", "build")
+	if err != nil {
+		return errors.Wrap(err, "error running kustomize")
+	}
+	// split out to the individual documents
+	yamls := strings.Split(output, "\n---\n")
+
+	for _, yamlstr := range yamls {
+		// extract all the .image values
+		var n yaml.Node
+
+		if err := yaml.Unmarshal([]byte(yamlstr), &n); err != nil {
+			return errors.Wrap(err, "error unmarshalling kustomize output")
+		}
+
+		path, err := yamlpath.NewPath(xpath)
+		if err != nil {
+			return errors.Wrap(err, "error creating yaml path")
+		}
+		elements, err := path.Find(&n)
+		if err != nil {
+			return errors.Wrap(err, "error finding image nodes")
+		}
+		for _, element := range elements {
+			i := strings.TrimSpace(element.Value)
+			if i != "" {
+				set.Add(i)
+			}
+		}
+
+	}
+	return nil
 }

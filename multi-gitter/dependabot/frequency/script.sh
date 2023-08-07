1cfa3bcfb01902be6907d29145091403c0700343
Mark deVilliers <mark.devilliers@adarga.ai>
2023-08-07 14:44:40 +0100

    fix spelling error

--- multi-gitter/dependabot/frequency/script.sh
+++ multi-gitter/dependabot/frequency/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# of there is a dependabot.yml file AND .package-ecosystem == "gomod change the interval to weekly
+# if there is a dependabot.yml file AND .package-ecosystem == "gomod change the interval to weekly
 if test -f .github/dependabot.yml ; then 
   yq -i '(.updates.[] | select(.package-ecosystem == "gomod") | .schedule.interval) |= "weekly"' .github/dependabot.yml
 fi

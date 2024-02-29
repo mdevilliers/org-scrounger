01a602c4c7ac8ee29e2d226632969cd12574e71e
Mark deVilliers <mark.devilliers@adarga.ai>
2024-02-29 10:07:49 +0000

    chore: update dependabot updater

--- multi-gitter/dependabot/script.sh
+++ multi-gitter/dependabot/script.sh
@@ -4,5 +4,5 @@ if test -f .github/dependabot.yml ; then
   # if package-ecosystem == Go, set schedule to weekly
   yq -i '(.updates.[] | select(.package-ecosystem == "gomod") | .schedule.interval) |= "weekly"' .github/dependabot.yml
   # if package-ecosystem == Go, add grouping for otel
-  yq -i '(.updates.[] | select(.package-ecosystem == "gomod")) += { "groups": { "otel": {"patterns": ["go.opentelemetry.io"]}, "adarga": {"patterns": ["github.com/Adarga-Ltd"] }}}' .github/dependabot.yml
+  yq -i '(.updates.[] | select(.package-ecosystem == "gomod")) += { "groups": { "production-dependencies": { "dependency-type": "production"}}}' .github/dependabot.yml
 fi
